6e8d107d624f7414d7ecac48f9b409a181702d11
zhanglei <zlennon@163.com>
2023-02-20 09:35:13 +0800

    add kafka

---
+++ common-entity/src/test/java/com/zlennon/commonentity/CommonEntityApplicationTests.java
@@ -0,0 +1,13 @@
+package com.zlennon.commonentity;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class CommonEntityApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
