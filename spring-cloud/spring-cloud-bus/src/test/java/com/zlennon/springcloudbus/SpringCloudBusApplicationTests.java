e4c343fd9d5929b3eda3fb675eb59449e527bc8e
zlennon <zlennon@163.com>
2023-02-26 19:51:12 +0800

    update spring-cloud-valut

---
+++ spring-cloud/spring-cloud-bus/src/test/java/com/zlennon/springcloudbus/SpringCloudBusApplicationTests.java
@@ -0,0 +1,13 @@
+package com.zlennon.springcloudbus;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SpringCloudBusApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
