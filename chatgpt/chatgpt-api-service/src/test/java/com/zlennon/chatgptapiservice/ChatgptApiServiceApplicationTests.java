481a7af1bba1c58a54a7dc08a94ba11590f84350
zhanglei <zlennon@163.com>
2023-03-02 22:03:54 +0800

    add spirng-cloud-stream

---
+++ chatgpt/chatgpt-api-service/src/test/java/com/zlennon/chatgptapiservice/ChatgptApiServiceApplicationTests.java
@@ -0,0 +1,13 @@
+package com.zlennon.chatgptapiservice;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ChatgptApiServiceApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
